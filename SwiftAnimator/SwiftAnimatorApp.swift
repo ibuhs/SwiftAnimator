c8304bb617449af086928fc9ce132cfa3c819a65
Shubham Kaila <github@shubhamkaila.com>
2024-11-29 16:22:53 -0500

    Initial Commit

---
+++ SwiftAnimator/SwiftAnimatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAnimatorApp.swift
+//  SwiftAnimator
+//
+//  Created by Jin on 2024-11-29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAnimatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftAnimatorTests/SwiftAnimatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAnimatorTests.swift
+//  SwiftAnimatorTests
+//
+//  Created by Jin on 2024-11-29.
+//
+
+import Testing
+@testable import SwiftAnimator
+
+struct SwiftAnimatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
